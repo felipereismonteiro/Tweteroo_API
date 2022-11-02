7efb2ed916af2cdd97f7447b58674a8bd7a6ccc2
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-11-02 09:59:33 -0300

    tweets sendo resgatados da api

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors"
+
+const tweets = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub",
+  },
+];
+
+const app = express();
+
+app.use(cors())
+
+app.get("/tweets", (req, res) => {
+    res.send(tweets)
+});
+
+app.listen(5000);
